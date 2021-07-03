323b6dbd8bca9f72cc7448f0d7774fd68bb9537f
jacarrichan <772929989@qq.com>
2021-07-03 17:44:35 +0800

    init

---
+++ src/test/java/com/jacarrichan/demo/rc/DemoTest.java
@@ -0,0 +1,35 @@
+package com.jacarrichan.demo.rc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+/**  指定当前生效的配置文件( active profile)，如果是 appplication-dev.yml 则 dev   **/
+@ActiveProfiles("dev")
+/** 指定  @SpringBootApplication  启动类 和 端口  **/
+@SpringBootTest(classes = DemoTest.ConfigContext.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class DemoTest {
+
+    @Value("${spring.application.name}")
+    public String applicationName;
+
+
+    @Test
+    public void test1() {
+        System.out.println(applicationName);
+
+    }
+
+    @Configuration()
+    @EnableAutoConfiguration()
+    @ComponentScan(value = {"com.jacarrichan.demo"})
+    static class ConfigContext {
+    }
+}
